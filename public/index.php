74025e770a67e1707daf2004a8b36634bd145336
Romario Flores <rflores@urbano.com.pe>
2024-05-31 14:33:25 -0500

    feature: refactoring

--- public/index.php
+++ public/index.php
@@ -1,12 +1,12 @@
 <?php
+// Define la constante BASE_PATH
+define('BASE_PATH', dirname(__DIR__));
+
 require_once BASE_PATH.'/vendor/autoload.php';
 require_once BASE_PATH.'/config/env.php';
 
 use App\Config\Database;
 use App\Controllers\Controller;
-
-// Define la constante BASE_PATH
-define('BASE_PATH', dirname(__DIR__));
 
 // Carga las variables de entorno
 $environment = EnvironmentLoader::load(BASE_PATH . '/.env');
