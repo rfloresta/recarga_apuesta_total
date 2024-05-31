cb3e6f29ded16233ba2c2db66f208ef4087a4a89
Romario Flores <rflores@urbano.com.pe>
2024-05-31 14:26:43 -0500

    feature: upd index.php

--- public/index.php
+++ public/index.php
@@ -1,32 +1,17 @@
 <?php
+require_once BASE_PATH.'/vendor/autoload.php';
+require_once BASE_PATH.'/config/env.php';
+
+use App\Config\Database;
+use App\Controllers\Controller;
 
 // Define la constante BASE_PATH
 define('BASE_PATH', dirname(__DIR__));
-
-require_once BASE_PATH.'/vendor/autoload.php';
-
-
-// Importa las clases necesarias
-require_once BASE_PATH.'/config/env.php';
-// use Config\EnvironmentLoader;
-
-use App\Config\Database;
-
-use App\DI\DIContainer;
-
-// Carga las rutas
-use App\Controllers\Controller;
 
 // Carga las variables de entorno
 $environment = EnvironmentLoader::load(BASE_PATH . '/.env');
 define('env', $environment);
 
-// $dbHost = $environment->get('DB_HOST');
-// $dbName = $environment->get('DB_NAME');
-// $dbUser = $environment->get('DB_USER');
-// $dbPass = $environment->get('DB_PASS');
-// Database::getInstance($dbHost, $dbName, $dbUser, $dbPass);
-
-// Crea una instancia del controlador y llama al método index
+// Cargamos las rutas
 $controller = new Controller();
 $controller->index();
